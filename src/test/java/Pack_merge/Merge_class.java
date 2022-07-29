cfb27f5898475520523518e9134db144af0d16a3
seeth <seeth@LAPTOP-NVUVQA1O>
2022-07-29 17:58:03 +0530

    ;

--- src/test/java/Pack_merge/Merge_class.java
+++ src/test/java/Pack_merge/Merge_class.java
@@ -3,7 +3,7 @@ package Pack_merge;
 public class Merge_class {
 	public static void main(String[] args) {
 		System.out.println("merge class1");
-		System.out.println("nagamani")
+		System.out.println("nagamani");
 	}
 
 }
