5e59498bdbca098ece6ed99eda8ab3f8ba68e3b1
pinja <pinja@DESKTOP-3BU923C>
2022-07-29 17:03:22 +0530

    camp commit

---
+++ src/test/java/Pack_Campaign/camp_1.java
@@ -0,0 +1,8 @@
+package Pack_Campaign;
+
+public class camp_1 {
+	public static void main(String[] args) {
+		System.out.println("camp_2");
+	}
+
+}
---
+++ src/test/java/Pack_Campaign/camp_2.java
@@ -0,0 +1,8 @@
+package Pack_Campaign;
+
+public class camp_2 {
+	public static void main(String[] args) {
+		System.out.println("camp_1");
+	}
+
+}
