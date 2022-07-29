2d9f47256b6ab21bc5104891f017d7b322cbb91e
pinja <pinja@DESKTOP-3BU923C>
2022-07-29 17:19:37 +0530

    merge commit

---
+++ src/test/java/Pack_merge/Merge_class.java
@@ -0,0 +1,8 @@
+package Pack_merge;
+
+public class Merge_class {
+	public static void main(String[] args) {
+		System.out.println("merge class1");
+	}
+
+}
---
+++ src/test/java/Pack_merge/Merge_class2.java
@@ -0,0 +1,8 @@
+package Pack_merge;
+
+public class Merge_class2 {
+	public static void main(String[] args) {
+		System.out.println("merge class2");
+	}
+
+}
